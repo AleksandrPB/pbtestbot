60f72d6e0fa9ad82471373fd806ccd3a108a1a89
Aleksandr Burlak <aburlaqqq@gmail.com>
2020-10-21 14:51:18 +0300

    revise command 'test'

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -39,37 +39,37 @@ class TestCommand extends Command
 
         $user = User::find(1);
 
-        $this->replyWithMessage(['text' => 'User email' . $user->email]);
+        $this->replyWithMessage(['text' => 'User email ' . $user->email]);
 
         $telegram_user = Telegram::getWebhookUpdates()['message'];
 
         $text = sprintf('%s: %s' . PHP_EOL, 'Chat id', $telegram_user['from']['id']);
         $text .= sprintf('%s: %s' . PHP_EOL, 'Username', $telegram_user['from']['username']);
 
-        $keyboard = [
-            ['7', '8', '9'],
-            ['4', '5', '6'],
-            ['1', '2', '3'],
-            ['0']
-        ];
-
-        $reply_markup = Telegram::replyKeyboardMarkup(
-            [
-                'keyboard' => $keyboard,
-                'resize_keyboard' => true,
-                'one_time_keyboard' => true
-            ]
-        );
-
-        $response = Telegram::sendMessage(
-            [
-                'chat_id' => $telegram_user['from']['id'],
-                'text' => 'Hello World',
-                'reply_markup' => $reply_markup
-            ]
-        );
-
-        $messageId = $response->getMessageId();
+//        $keyboard = [
+//            ['7', '8', '9'],
+//            ['4', '5', '6'],
+//            ['1', '2', '3'],
+//            ['0']
+//        ];
+//
+//        $reply_markup = Telegram::replyKeyboardMarkup(
+//            [
+//                'keyboard' => $keyboard,
+//                'resize_keyboard' => true,
+//                'one_time_keyboard' => true
+//            ]
+//        );
+//
+//        $response = Telegram::sendMessage(
+//            [
+//                'chat_id' => $telegram_user['from']['id'],
+//                'text' => 'Hello World',
+//                'reply_markup' => $reply_markup
+//            ]
+//        );
+//
+//        $messageId = $response->getMessageId();
 
         $this->replyWithMessage(compact('text'));
     }
