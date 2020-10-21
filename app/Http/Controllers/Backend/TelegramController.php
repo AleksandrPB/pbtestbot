6f9be30811b2fd8b4fd93547bd65ec0331942f8f
Aleksandr Burlak <aburlaqqq@gmail.com>
2020-10-21 17:55:44 +0300

    model and migration for bot-users

--- app/Http/Controllers/Backend/TelegramController.php
+++ app/Http/Controllers/Backend/TelegramController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\TelegramUser;
 use Illuminate\Http\Request;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -10,6 +11,11 @@ class TelegramController extends Controller
 {
     public function webhook()
     {
+        $telegram = Telegram::getWebhookUpdates()['message'];
+
+        if (!TelegramUser::find($telegram['from']['id'])) {
+            TelegramUser::create(json_decode($telegram['from'], true));
+        }
         Telegram::commandsHandler(true);
     }
 }
